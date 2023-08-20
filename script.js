33272bd19bed1b80d2ef260c26f5de4896407c4d
TanimShahriar <ssmtsshr@gmail.com>
2023-08-20 10:53:56 +0600

    make purchase and apply button enabled when the price is changed

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 let total = 0;
 
-function handleClickBtn(target) {
+
+function handleClick(target) {
+
   const selectedItemsContainer = document.getElementById('selectedItems');
-  const itemName = target.parentNode.childNodes[5].innerText;
-  const li = document.createElement('h1');
+  // const itemName = target.parentNode.childNodes[3].childNodes[7].innerText;
+  const itemName = target.childNodes[7].innerText;
+
+  console.log(target.childNodes[7].innerText);
+  const li = document.createElement('p');
   li.innerText = itemName;
   selectedItemsContainer.appendChild(li);
 
-  const priceString = target.parentNode.childNodes[13].innerText.split(' ')[1];
-  const price = parseInt(priceString);
+  const priceString = target.childNodes[9].innerText.split(' ')[0];
+  console.log(target.childNodes[9].innerText.split(' ')[0]);
+  const price = parseFloat(priceString);
   total += price;
-  document.getElementById('greenBoxTotal').innerText = total;
-  updateGrandTotal();
+  document.getElementById('totalPrice').innerText = total;
+  finalTotalPrice();
 }
 
-function updateGrandTotal() {
-  const grandTotalElement = document.getElementById('grandTotal');
-  grandTotalElement.innerText = total;
+
+function finalTotalPrice() {
+  const finalTotalPrice = document.getElementById('finalPrice');
+  finalTotalPrice.innerText = total;
+
+  const makePurchaseButton = document.getElementById('makePurchaseButton');
+  makePurchaseButton.disabled = total <= 0;
+
+  const applyButton = document.getElementById('applyButton');
+  applyButton.disabled = total < 200;
 }
+
+
+
+
+
